964ee446e43143ed2251ec714e6bc32b6c41a3da
Dan Tech <danhtran.dev@outlook.com>
2024-02-04 21:30:23 +0700

    Update main.kt

--- src/main/kotlin/com/theapache64/klokk/main.kt
+++ src/main/kotlin/com/theapache64/klokk/main.kt
@@ -24,7 +24,7 @@ import java.util.Calendar
 
 const val COLUMNS = 15
 const val ROWS = 8
-const val PADDING = 500
+const val PADDING = 100
 const val CLOCK_SIZE = 60
 const val CLOCKS_CONTAINER_WIDTH = CLOCK_SIZE * COLUMNS
 const val CLOCKS_CONTAINER_HEIGHT = CLOCK_SIZE * ROWS
